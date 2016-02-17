7571d0a5e72a398251b603ce7aaa95980d6cf650
Matthew Lindsay <thespian42@gmail.com>
2016-02-17 16:32:20 -0500

    Added preliminary rectangle and line code

--- Rectangle_Exercise/Line.cs
+++ Rectangle_Exercise/Line.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,48 @@ namespace Rectangle_Exercise
 	{
 		//In a rendering engine, a texture referes to a collection of pixel data used to render an object.
 		Texture2D lineTex;
+		Vector2 startPoint;
+		Vector2 endPoint;
 
-		public Line(float startPoint, float endPoint)
+		
+		public Color color
 		{
+			get
+			{
+				return color;
+			}
+			set
+			{
+				color = value;
+			}
+		} 
 
-			
+		//The Sprite Batch is a way of communicating with the renderer.
+		SpriteBatch sb;
+
+		public Line(Vector2 startPointArg, Vector2 endPointArg, Color rectColor, GraphicsDevice gd)
+		{
+			startPoint = startPointArg;
+			endPoint = endPointArg;
+			color = rectColor;
+
+			//Here, we are making a texture which is a single pixel. This will be stretched to form our lines.
+			lineTex = new Texture2D(gd, 1, 1);
+
+			//The texture must be sent an array of colors, each index representing one pixel. 
+			lineTex.SetData<Color>(new Color[] { color });
+			sb = new SpriteBatch(gd);//This points the spritebatch to the renderer to allow us to make draw calls.
+		}
+
+		public void Draw()
+		{
+			Vector2 edge = endPoint - startPoint;
+			float angle = (float)Math.Atan2(edge.Y, edge.X);
+
+			//We are using the rectangle here as an arugment to render the line. This is necessary to achieve the solid lines
+			//and transparent interior to each rectangle which these lines are a part of.
+			sb.Draw(lineTex, new Rectangle((int)startPoint.X, (int)startPoint.Y, (int)edge.Length(), 1), null, color, angle, new Vector2(0, 0), SpriteEffects.None, 0);
+
 		}
 	}
 }
---
+++ Rectangle_Exercise/Rect.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Exercise
+{
+	//NOTE: We will not be using any of the functions of the built in Rectangle object, as that would defeat the purpose of the exercise.
+	class Rect
+	{
+		//These will be our primary detection tools, and are extensible to rotated rectangle detection.
+		Vector2 topLeft;
+		Vector2 topRight;
+		Vector2 botLeft;
+		Vector2 botRight;
+
+		Line top;
+		Line right;
+		Line left;
+		Line bot;
+
+		Color color;
+
+		Texture2D interiorTex;
+
+		SpriteBatch sb;
+
+		//This will be used to determine the initial placement and size.
+		Random rand = new Random();
+
+		public Rect(GraphicsDevice gd)
+		{
+			color = Color.Green; //The rectangle is green when no intersections, adjacencies, or containments are occuring.
+			int height = rand.Next(1, 150);
+			int width = rand.Next(1, 150);
+
+			topLeft = new Vector2(rand.Next(10, 400), rand.Next(10, 400));
+			topRight = new Vector2(topLeft.X + width, topLeft.Y);
+			botLeft = new Vector2(topLeft.X, topLeft.Y + height);
+			botRight = new Vector2(topLeft.X + width, topLeft.Y + height);
+
+			interiorTex = new Texture2D(gd, width, height);
+			Color[] colorData = new Color[width * height];
+
+			for (int i = 0; i < colorData.Length; ++i)
+			{
+				colorData[i] = color;
+			}
+
+			interiorTex.SetData<Color>(colorData);
+		}
+	}
+}
