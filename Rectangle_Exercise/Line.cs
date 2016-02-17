1dad3fd59a33d6c0dc87a27253ed99c2ed9b917d
Matthew Lindsay <thespian42@gmail.com>
2016-02-17 13:55:47 -0500

    Initial commit
    Contains a Monogame project with settings applied to compile targeting
    .NET 4.5

---
+++ Rectangle_Exercise/Line.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Exercise
+{
+	class Line
+	{
+		//In a rendering engine, a texture referes to a collection of pixel data used to render an object.
+		Texture2D lineTex;
+
+		public Line(float startPoint, float endPoint)
+		{
+
+			
+		}
+	}
+}
