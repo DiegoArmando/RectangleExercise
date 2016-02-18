f7dbea9bdcd9e507f4633a82bab47bc914fe61c0
Matthew Lindsay <thespian42@gmail.com>
2016-02-18 00:56:43 -0500

    Added overlap

--- Rectangle_Exercise/Line.cs
+++ Rectangle_Exercise/Line.cs
@@ -15,27 +15,44 @@ namespace Rectangle_Exercise
 		Vector2 startPoint;
 		Vector2 endPoint;
 
-		
-		public Color color
-		{
-			get
-			{
-				return color;
-			}
-			set
-			{
-				color = value;
-			}
-		} 
+		Vector2 initialStart;
+		Vector2 initialEnd;
+
+		int lineWidth = 2;
+
+		public Color color;
+
+		//Used to reset line position correctly on the border of the rectangles
+		bool shiftR;
+		bool shiftU;
 
 		//The Sprite Batch is a way of communicating with the renderer.
 		SpriteBatch sb;
 
-		public Line(Vector2 startPointArg, Vector2 endPointArg, Color rectColor, GraphicsDevice gd)
+		public Line(Vector2 startPointArg, Vector2 endPointArg, Color rectColor, GraphicsDevice gd, bool shiftRight, bool shiftUp)
 		{
 			startPoint = startPointArg;
 			endPoint = endPointArg;
+
+			initialStart = startPoint;
+			initialEnd = endPoint;
+
 			color = rectColor;
+			color.A = 255; //nontransparent lines
+
+			shiftR = shiftRight;
+			shiftU = shiftUp;
+
+			if(shiftR)
+			{
+				startPoint.X += lineWidth;
+				endPoint.X += lineWidth;
+			}
+			if(shiftU)
+			{
+				startPoint.Y -= lineWidth;
+				endPoint.Y -= lineWidth;
+			}
 
 			//Here, we are making a texture which is a single pixel. This will be stretched to form our lines.
 			lineTex = new Texture2D(gd, 1, 1);
@@ -47,13 +64,42 @@ namespace Rectangle_Exercise
 
 		public void Draw()
 		{
+			sb.Begin();
 			Vector2 edge = endPoint - startPoint;
 			float angle = (float)Math.Atan2(edge.Y, edge.X);
 
 			//We are using the rectangle here as an arugment to render the line. This is necessary to achieve the solid lines
 			//and transparent interior to each rectangle which these lines are a part of.
-			sb.Draw(lineTex, new Rectangle((int)startPoint.X, (int)startPoint.Y, (int)edge.Length(), 1), null, color, angle, new Vector2(0, 0), SpriteEffects.None, 0);
+			sb.Draw(lineTex, new Rectangle((int)startPoint.X, (int)startPoint.Y, (int)edge.Length(), lineWidth), null, color, angle, new Vector2(0, 0), SpriteEffects.None, 1);
 
+
+			sb.End();
+		}
+
+		public void UpdatePosition(Vector2 difference)
+		{
+			startPoint = initialStart + difference;
+			endPoint = initialEnd + difference;
+		}
+
+		//Called after the box is dropped by the user to prevent resetting the position incorrectly on a second pickup.
+		public void SetNewHome()
+		{
+			initialStart = startPoint;
+			initialEnd = endPoint;
+
+			if (shiftR)
+			{
+				startPoint.X += lineWidth;
+				endPoint.X += lineWidth;
+			}
+			if (shiftU)
+			{
+				startPoint.Y -= lineWidth;
+				endPoint.Y -= lineWidth;
+			}
+
+			
 		}
 	}
 }
--- Rectangle_Exercise/Rect.cs
+++ Rectangle_Exercise/Rect.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Rectangle_Exercise
 {
@@ -12,10 +13,14 @@ namespace Rectangle_Exercise
 	class Rect
 	{
 		//These will be our primary detection tools, and are extensible to rotated rectangle detection.
-		Vector2 topLeft;
-		Vector2 topRight;
-		Vector2 botLeft;
-		Vector2 botRight;
+		public Vector2 topLeft;
+		public Vector2 topRight;
+		public Vector2 botLeft;
+		public Vector2 botRight;
+
+		//The rectangle's upper left corner's position on the screen
+		Vector2 pos;
+		Vector2 initialPos;
 
 		Line top;
 		Line right;
@@ -28,22 +33,35 @@ namespace Rectangle_Exercise
 
 		SpriteBatch sb;
 
+		int height;
+		int width;
+
+		//This is a series of bools instead of an enumeration so that combinations of states can be tracked.
+		public bool intersecting = false;
+		public bool containing = false;
+		public bool contained = false;
+		public bool adjacent = false;
+
+		//Is the box being dragged around the screen by the mouse?
+		bool beingDragged = false;
+		Vector2 initDragPoint;//where the mouse was when the box was clicked
+
 		//This will be used to determine the initial placement and size.
-		Random rand = new Random();
+		//While this may look excessive, usng the default rand() may result in duplicate values 
+		//upon rapid generation.
+		RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+		byte[] randomNumber = new byte[1];
 
 		public Rect(GraphicsDevice gd)
 		{
-			color = Color.Green; //The rectangle is green when no intersections, adjacencies, or containments are occuring.
-			int height = rand.Next(1, 150);
-			int width = rand.Next(1, 150);
+			sb = new SpriteBatch(gd);
 
-			topLeft = new Vector2(rand.Next(10, 400), rand.Next(10, 400));
-			topRight = new Vector2(topLeft.X + width, topLeft.Y);
-			botLeft = new Vector2(topLeft.X, topLeft.Y + height);
-			botRight = new Vector2(topLeft.X + width, topLeft.Y + height);
+			color = Color.White; //The color is set to white so that it can be shifted any way we like.
+			color.A = 122;//set the alpha value to about half
 
-			interiorTex = new Texture2D(gd, width, height);
-			Color[] colorData = new Color[width * height];
+
+			interiorTex = new Texture2D(gd, 1, 1);
+			Color[] colorData = new Color[1];
 
 			for (int i = 0; i < colorData.Length; ++i)
 			{
@@ -51,6 +69,172 @@ namespace Rectangle_Exercise
 			}
 
 			interiorTex.SetData<Color>(colorData);
+
+			Console.Write("sb test in initialize: " + sb.ToString());
+
+			//Sets the size and position
+			Refresh(gd);
+			
+		}
+
+		public void Refresh(GraphicsDevice gd)
+		{
+			rand.GetBytes(randomNumber);
+			height = Convert.ToInt32(randomNumber[0]) % 225 + 25;
+			rand.GetBytes(randomNumber);
+			width = Convert.ToInt32(randomNumber[0]) % 225 + 25;
+
+			color = Color.White;
+			color.A = 122;
+
+			topLeft = new Vector2();
+			rand.GetBytes(randomNumber);
+			topLeft.X = Convert.ToInt32(randomNumber[0]) % 500 + 1;
+
+			rand.GetBytes(randomNumber);
+			topLeft.Y = Convert.ToInt32(randomNumber[0]) % 500 + 1;
+
+			topRight = new Vector2(topLeft.X + width, topLeft.Y);
+			botLeft = new Vector2(topLeft.X, topLeft.Y + height);
+			botRight = new Vector2(topLeft.X + width, topLeft.Y + height);
+
+			top = new Line(topLeft, topRight, color, gd, false, true);
+			left = new Line(topLeft, botLeft, color, gd, false, false);
+			right = new Line(topRight, botRight, color, gd, true, false);
+			bot = new Line(botLeft, botRight, color, gd, false, false);
+
+			pos = topLeft;
+			initialPos = pos;
+
+		}
+
+		public void Update(bool mouseDown, Point mousePos, ref bool dragging)
+		{
+			Vector2 mouseVector = new Vector2(mousePos.X, mousePos.Y);
+			//If the mouse is down and over the rectangle:
+			if (mouseDown)
+			{
+				if (ContainsPoint(mouseVector))
+				{
+					if (!beingDragged && !dragging)
+					{
+						beingDragged = true;
+						initDragPoint = mouseVector;
+						dragging = true;
+					}
+				}
+
+				if (beingDragged)
+				{
+					UpdatePosition(mouseVector - initDragPoint);
+				}
+			}
+			else
+			{
+				if (beingDragged)
+				{
+					beingDragged = false;
+					dragging = false;
+					initialPos = pos;
+					top.SetNewHome();
+					left.SetNewHome();
+					right.SetNewHome();
+					bot.SetNewHome();
+				}
+			}
+
+
+			//Color is priorized based on state
+			if (contained)
+			{
+				ChangeColor(Color.Black);
+			}
+			else if (containing)
+			{
+				ChangeColor(Color.Purple);
+			}
+			else if(adjacent)
+			{
+				ChangeColor(Color.Yellow);
+			}
+			else if(intersecting)
+			{
+				ChangeColor(Color.Blue);
+			}
+			else if(beingDragged)
+			{
+				ChangeColor(new Color(color.R + 30, color.G + 30, color.B + 30, color.A));
+			}
+			else
+			{
+				ChangeColor(Color.Green);
+			}
+
+			//Set all flags as false at the end of update to avoid costly and cumbersome calculations.
+			//These will be updated as needed in CheckInteractions, which is called after update.
+			intersecting = false;
+			containing = false;
+			contained = false;
+			adjacent = false;
+		}
+
+		private void UpdatePosition(Vector2 difference)
+		{
+			//update the position of topLeft, topRight, botLeft and botRight by the difference
+
+			pos = initialPos + difference;
+			topLeft = pos;
+			topRight = new Vector2(topLeft.X + width, topLeft.Y);
+			botLeft = new Vector2(topLeft.X, topLeft.Y + height);
+			botRight = new Vector2(topLeft.X + width, topLeft.Y + height);
+
+			top.UpdatePosition(difference);
+			left.UpdatePosition(difference);
+			right.UpdatePosition(difference);
+			bot.UpdatePosition(difference);
+
+
+		}
+
+		public void checkInteraction(Rect otherRect)
+		{
+			if(ContainsPoint(otherRect.topLeft) && ContainsPoint(otherRect.topRight) && ContainsPoint(otherRect.botLeft) && ContainsPoint(otherRect.botRight))
+			{
+				containing = true;
+				otherRect.contained = true;
+			}
+		}
+
+		public bool ContainsPoint(Vector2 testPoint)
+		{
+			//Check if the point is in between the sides of the rectangle
+			return (testPoint.Y > topLeft.Y && testPoint.Y < botRight.Y && testPoint.X > topLeft.X && testPoint.X < botRight.X);
+		}
+
+		void ChangeColor(Color newColor)
+		{
+			color = newColor;
+			color.A = 122;
+			top.color = newColor;
+			left.color = newColor;
+			right.color = newColor;
+			bot.color = newColor;
+		}
+
+		public void Draw()
+		{
+			sb.Begin();
+			int width = (int)(topRight - topLeft).Length();
+			int height = (int)(botLeft - topLeft).Length();
+
+			sb.Draw(interiorTex, new Rectangle((int)topLeft.X, (int)topLeft.Y, width, height), null, color, 0, new Vector2(0, 0), SpriteEffects.None, 0);
+
+			//top.Draw();
+			//left.Draw();
+			//right.Draw();
+			//bot.Draw();
+			
+			sb.End();
 		}
 	}
 }
